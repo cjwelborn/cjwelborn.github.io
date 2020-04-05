b0db368a00754cb4123f1f6307b88780e499200a
Christopher Welborn <cj@welbornprod.com>
2020-04-05 16:04:08 -0500

    colrc: true debian packages.

--- colrc/development.js
+++ colrc/development.js
@@ -6,25 +6,26 @@ var development =
       [ "Python", "dependencies.html#autotoc_md4", null ]
     ] ],
     [ "Tests", "tests.html", [
-      [ "About", "tests.html#autotoc_md25", null ],
-      [ "Basic Test:", "tests.html#autotoc_md26", null ],
-      [ "Memcheck Test:", "tests.html#autotoc_md27", null ],
-      [ "Quick Testing", "tests.html#autotoc_md28", null ],
-      [ "Test Everything", "tests.html#autotoc_md29", null ],
-      [ "Test Tool", "tests.html#autotoc_md30", null ]
+      [ "About", "tests.html#autotoc_md30", null ],
+      [ "Basic Test:", "tests.html#autotoc_md31", null ],
+      [ "Memcheck Test:", "tests.html#autotoc_md32", null ],
+      [ "Quick Testing", "tests.html#autotoc_md33", null ],
+      [ "Test Everything", "tests.html#autotoc_md34", null ],
+      [ "Test Tool", "tests.html#autotoc_md35", null ]
     ] ],
     [ "Make", "make.html", [
-      [ "ColrC Make Targets", "make.html#autotoc_md20", null ],
-      [ "Build", "make.html#autotoc_md21", null ],
-      [ "Test", "make.html#autotoc_md22", null ],
-      [ "Document", "make.html#autotoc_md23", null ],
-      [ "Examples", "make.html#autotoc_md24", null ]
+      [ "ColrC Make Targets", "make.html#autotoc_md24", null ],
+      [ "Build", "make.html#autotoc_md25", null ],
+      [ "Test", "make.html#autotoc_md26", null ],
+      [ "Document", "make.html#autotoc_md27", null ],
+      [ "Examples", "make.html#autotoc_md28", null ],
+      [ "Debian Packages", "make.html#autotoc_md29", null ]
     ] ],
     [ "Tools", "tools.html", [
-      [ "ColrC Tools", "tools.html#autotoc_md40", null ]
+      [ "ColrC Tools", "tools.html#autotoc_md45", null ]
     ] ],
     [ "Examples", "examples_dev.html", [
-      [ "ColrC Examples", "examples_dev.html#autotoc_md10", null ]
+      [ "ColrC Examples", "examples_dev.html#autotoc_md14", null ]
     ] ],
     [ "Compatibility", "compatibility.html", [
       [ "About", "compatibility.html#autotoc_md0", null ],
--- colrc/tool.js
+++ colrc/tool.js
@@ -1,15 +1,15 @@
 var tool =
 [
-    [ "About", "tool.html#autotoc_md31", [
-      [ "Colorizing Text", "tool.html#autotoc_md32", null ],
-      [ "Rainbows", "tool.html#autotoc_md33", null ],
-      [ "Stripping Colorized Output", "tool.html#autotoc_md34", null ],
-      [ "Inspecting Colorized Output", "tool.html#autotoc_md35", null ],
-      [ "Translating Color Codes", "tool.html#autotoc_md36", null ]
+    [ "About", "tool.html#autotoc_md36", [
+      [ "Colorizing Text", "tool.html#autotoc_md37", null ],
+      [ "Rainbows", "tool.html#autotoc_md38", null ],
+      [ "Stripping Colorized Output", "tool.html#autotoc_md39", null ],
+      [ "Inspecting Colorized Output", "tool.html#autotoc_md40", null ],
+      [ "Translating Color Codes", "tool.html#autotoc_md41", null ]
     ] ],
     [ "Tool Building", "tool_building.html", [
-      [ "Build", "tool_building.html#autotoc_md37", null ],
-      [ "Install", "tool_building.html#autotoc_md38", null ],
-      [ "Uninstall", "tool_building.html#autotoc_md39", null ]
+      [ "Build", "tool_building.html#autotoc_md42", null ],
+      [ "Install", "tool_building.html#autotoc_md43", null ],
+      [ "Uninstall", "tool_building.html#autotoc_md44", null ]
     ] ]
 ];
